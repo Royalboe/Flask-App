f3fd54753e0dd8cd704917497ea2d0019b9f37ad
Ayomide Salami <auomidejohnson@gmail.com>
2022-12-01 23:36:34 +0000

    added index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Home Page'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return "About Page"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
